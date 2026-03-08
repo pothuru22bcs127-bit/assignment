b4de79aba7e46032173584bb9b91b90f42d6121a
pothuru22bcs127-bit <pothuru22bcs127@iiitkottayam.ac.in>
2026-03-08 13:58:41 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,75 +1,44 @@
 import pandas as pd
 import os
 import datetime
+import joblib
+
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import f1_score, roc_auc_score, precision_score
-import joblib
 
 from preprocess import preprocess_data
 
-print("========== MLOps Training Pipeline Started ==========")
+print("Training Pipeline Started")
 
-# Load dataset
-print("Loading dataset...")
 df = pd.read_csv("Telco_customer_churn.csv")
 
 print("Dataset Shape:", df.shape)
 
-# Preprocess data
-print("Preprocessing data...")
 X, y = preprocess_data(df)
 
-# Train-test split
 X_train, X_test, y_train, y_test = train_test_split(
     X, y,
     test_size=0.2,
-    random_state=42,
-    stratify=y
+    random_state=42
 )
 
-print("Training Random Forest Model...")
-
-# Train model
-model = RandomForestClassifier(n_estimators=100, random_state=42)
+model = RandomForestClassifier(n_estimators=100)
 model.fit(X_train, y_train)
 
-print("Model Training Completed!")
-
-# Predictions
 preds = model.predict(X_test)
 
-# Probability handling
-probs = model.predict_proba(X_test)
+probs = model.predict_proba(X_test)[:,1]
 
-if probs.shape[1] > 1:
-    probs = probs[:, 1]
-else:
-    probs = probs[:, 0]
-
-# Evaluation Metrics
-print("========== Model Metrics ==========")
-
-f1 = f1_score(y_test, preds)
-roc_auc = roc_auc_score(y_test, probs)
-precision = precision_score(y_test, preds)
-
-print(f"F1 Score: {f1:.4f}")
-print(f"ROC-AUC Score: {roc_auc:.4f}")
-print(f"Precision Score: {precision:.4f}")
-
-print("====================================")
-
-# ---------- Model Versioning ----------
+print("Metrics:")
+print("F1:", f1_score(y_test, preds))
+print("ROC-AUC:", roc_auc_score(y_test, probs))
+print("Precision:", precision_score(y_test, preds))
 
 os.makedirs("models", exist_ok=True)
 
 version = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
-model_path = f"models/model_{version}.pkl"
+joblib.dump(model, f"models/model_{version}.pkl")
 
-joblib.dump(model, model_path)
-
-print("Model saved at:", model_path)
-
-print("========== Training Pipeline Finished ==========")
+print("Model saved")
