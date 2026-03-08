aa878aa8bbe5c3cffe44716c2bc40f5e2f7800fe
pothuru22bcs127-bit <pothuru22bcs127@iiitkottayam.ac.in>
2026-03-08 11:27:21 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -14,9 +14,11 @@ X, y = preprocess_data(df)
 
 # split
 X_train, X_test, y_train, y_test = train_test_split(
-    X, y, test_size=0.2, random_state=42
+    X, y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
 )
-
 # model
 model = RandomForestClassifier()
 
